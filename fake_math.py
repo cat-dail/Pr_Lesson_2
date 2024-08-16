b8ba04014f7457c8239e0333af63df5a3d49b154
cat-dail <cat_dail@mail.ru>
2024-08-16 18:15:16 +0300

    Add files via upload
    Домашняя работа по уроку "Модули и пакеты"

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+    str = 'Ошибка'
+    if second == 0:
+        return str
+    else:
+        a = first / second
+    return a
---
+++ modul4_1.py
@@ -0,0 +1,13 @@
+# Домашняя работа по уроку "Модули и пакеты"
+import fake_math as fake_ma
+import true_math as tr_ma
+
+
+result1 = fake_ma.divide(69, 3)
+result2 = fake_ma.divide(3, 0)
+result3 = tr_ma.divide(49, 7)
+result4 = tr_ma.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)

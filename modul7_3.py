e802c2dcf430529794b32666f28930ff10aa310c
cat-dail <cat_dail@mail.ru>
2024-10-08 19:02:47 +0300

    Add files via upload
    Домашнее задание по теме "Оператор "with".

---
+++ modul7_3.py
@@ -0,0 +1,102 @@
+# Цель: применить на практике оператор with, вспомнить написание кода в парадигме ООП.
+#
+# Задача "Найдёт везде":
+# Напишите класс WordsFinder, объекты которого создаются следующим образом:
+# WordsFinder('file1.txt, file2.txt', 'file3.txt', ...).
+# Объект этого класса должен принимать при создании неограниченного количество названий файлов
+# и записывать их в атрибут file_names в виде списка или кортежа.
+#
+# Также объект класса WordsFinder должен обладать следующими методами:
+# get_all_words - подготовительный метод, который возвращает словарь следующего вида:
+# {'file1.txt': ['word1', 'word2'], 'file2.txt': ['word3', 'word4'], 'file3.txt': ['word5', 'word6', 'word7']}
+# Где:
+# 'file1.txt', 'file2.txt', ''file3.txt'' - названия файлов.
+# ['word1', 'word2'], ['word3', 'word4'], ['word5', 'word6', 'word7'] - слова содержащиеся в этом файле.
+# Алгоритм получения словаря такого вида в методе get_all_words:
+# Создайте пустой словарь all_words.
+# Переберите названия файлов и открывайте каждый из них, используя оператор with.
+# Для каждого файла считывайте единые строки, переводя их в нижний регистр (метод lower()).
+# Избавьтесь от пунктуации [',', '.', '=', '!', '?', ';', ':', ' - '] в строке.
+# (тире обособлено пробелами, это не дефис в слове).
+# Разбейте эту строку на элементы списка методом split(). (разбивается по умолчанию по пробелу)
+# В словарь all_words запишите полученные данные, ключ - название файла, значение - список из слов этого файла.
+#
+# find(self, word) - метод, где word - искомое слово. Возвращает словарь, где ключ - название файла,
+# значение - позиция первого такого слова в списке слов этого файла.
+# count(self, word) - метод, где word - искомое слово. Возвращает словарь, где ключ - название файла,
+# значение - количество слова word в списке слов этого файла.
+# В методах find и count пользуйтесь ранее написанным методом get_all_words для получения названия файла
+# и списка его слов.
+# Для удобного перебора одновременно ключа(названия) и значения(списка слов) можно
+# воспользоваться методом словаря - item().
+#
+# for name, words in get_all_words().items():
+#   # Логика методов find или count
+#
+# Пример результата выполнения программы:
+# Представим, что файл 'test_file.txt' содержит следующий текст:
+# It's the text for task Найти везде
+# Используйте его для самопроверки
+# Успехов в рещении задач!
+# text text text
+# Пример выполнения программы:
+# finder2 = WordsFinder('test_file.txt')
+# print(finder2.get_all_words()) # Все слова
+# print(finder2.find('TEXT')) # 3 слово по счёту
+# print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+#
+# Вывод на консоль:
+# {'test_file.txt': ["it's", 'a', 'text', 'for', 'task', 'найти', 'везде',
+# 'используйте', 'его', 'для', 'самопроверки',
+# 'успехов', 'в', 'решении', 'задачи', 'text', 'text', 'text']}
+# {'test_file.txt': 3}
+# {'test_file.txt': 4}
+
+
+class WordsFinder:
+
+    def __init__(self, *file_name: str):
+        self.file_name = file_name
+
+    def get_all_words(self):
+        all_words = {}
+        for file_name in self.file_name:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                words = []
+                for line in file.readlines():
+                    line = line.strip().lower()
+                    for symvol in [',', '.', '=', '!', '?', ';', ':', ' -']:
+                        line = line.replace(symvol, "")
+                    words.extend(line.split(" "))
+            all_words[self.file_name] = words
+        return all_words
+
+    def find(self, word):
+        all_words = self.get_all_words()
+        result = {}
+        for file, words in all_words.items():
+            if word.lower() in words:
+                result[file] = words.index(word.lower())
+        return result
+
+    def count(self, word):
+        all_words = self.get_all_words()
+        result = {}
+        for file, words in all_words.items():
+            if word.lower() in words:
+                result[file] = words.count(word.lower())
+        return result
+
+   
+finder2 = WordsFinder('test_file.txt')
+
+
+print(finder2.get_all_words())  # Все слова
+print(finder2.find('TEXT'))  # 3 слово по счёту
+print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
+
+# test_file.txt
+# It'is a TEXT for task: Найти везде,
+# Используйте его для самопроверки.
+# Успехов в решении задач!
+# text text - text?

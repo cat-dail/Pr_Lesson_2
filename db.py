56c43b45c95228908a8de55481396cd5065b55da
cat-dail <cat_dail@mail.ru>
2025-01-22 21:41:22 +0300

    Add files via upload
    Задача "Модели SQLAlchemy"   файл в backend

---
+++ db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = "sqlite:///taskmanager.db" # Путь к базе данных SQLite
+# Создаём движок для подключения к базе данных
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Создаём фабрику сессий для работы с базой данных
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Базовый класс для всех моделей SQLAlchemy
+Base = declarative_base()

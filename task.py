47aa2ec9a8a56a5b8e6fe7419895a00e431f6ea4
cat-dail <cat_dail@mail.ru>
2025-01-22 21:51:29 +0300

    Update task.py

--- task.py
+++ task.py
@@ -9,8 +9,8 @@ class Task(Base):
     __tablename__ = "tasks" # Имя таблицы в базе данных
     __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True, index=True) # Уникальный идентификатор
-    title = Column(String) # Название категории
-    content = Column(String) # Человекочитаемый URL
+    title = Column(String) 
+    content = Column(String) 
     priority = Column(Integer, default=0)
     completed = Column(Boolean, default=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False) # Связь с User
